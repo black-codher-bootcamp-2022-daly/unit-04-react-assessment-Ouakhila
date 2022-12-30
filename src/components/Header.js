7359f1b88294b83e5519e052509026e0edb8f384
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2022-12-30 12:51:33 +0000

    checking autograding

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,17 @@ import { Link } from "react-router-dom";
 function Header(props) {
   return (
     <div className="Nav-element">
-      <ul className="Nav-bar-list">
-        <Link to="/" id="homelink" className="List">
-          Home
-        </Link>
+      <Link to="/" id="homelink" className="List">
+        Home
+      </Link>
 
-        <Link to="/about" id="aboutlink" className="List">
-          About
-        </Link>
+      <Link to="/about" id="aboutlink" className="List">
+        About
+      </Link>
 
-        <Link to="/basket" id="basketlink" className="List">
-          Basket: {props.itemCount} items
-        </Link>
-      </ul>
+      <Link to="/basket" id="basketlink" className="List">
+        Basket: {props.itemCount} items
+      </Link>
     </div>
   );
 }
