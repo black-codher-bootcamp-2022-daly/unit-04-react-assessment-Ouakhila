aa17950bbc141c96514548d8bc7d929a1c09f2d7
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2023-01-05 09:23:28 +0000

    checking total

--- src/components/BasketTotal.js
+++ src/components/BasketTotal.js
@@ -1,7 +1,7 @@
 import React from "react";
 function BasketTotal(props) {
   //console.log(props.basketTotal);
-  return <>Total: {props.basketTotal} </>;
+  return <h1 className="total">Total: {props.basketTotal} </h1>;
 }
 
 export default BasketTotal;
